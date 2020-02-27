5882f436720cb317784630bf5b585e9d0e1cab72
Al Yin <altyin@gmail.com>
2020-02-27 16:42:26 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/prototype/precache-manifest.542f05c1f25a6aca6810420c3dc135a7.js"
+  "/prototype/precache-manifest.d10dbe2c13eb884e3506964420b743ce.js"
 );
 
 workbox.clientsClaim();
