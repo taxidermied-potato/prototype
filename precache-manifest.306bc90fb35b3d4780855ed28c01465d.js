2160e92d8cecbd37c06a5763da9ea5af0af1b54c
Al Yin <altyin@gmail.com>
2020-02-27 15:59:42 -0500

    Updates

---
+++ precache-manifest.306bc90fb35b3d4780855ed28c01465d.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "c9a6aeefaff3518f037214d7e67d2de6",
+    "url": "/prototype/static/media/postspage.c9a6aeef.png"
+  },
+  {
+    "revision": "5d8d1e53e2a23e9b5108e07e0c8a3e84",
+    "url": "/prototype/static/media/loginscreen1.5d8d1e53.png"
+  },
+  {
+    "revision": "c5be282aaf3fda4236325f406d9f2665",
+    "url": "/prototype/static/media/loginscreen.c5be282a.png"
+  },
+  {
+    "revision": "04d4c51228ed802e72f3054f45adfd69",
+    "url": "/prototype/static/media/PBack.04d4c512.png"
+  },
+  {
+    "revision": "07f3422638c6c5215d8f",
+    "url": "/prototype/static/js/runtime~main.d68e0bf9.js"
+  },
+  {
+    "revision": "2abbcc0378228d296b9d",
+    "url": "/prototype/static/js/main.c9844fcc.chunk.js"
+  },
+  {
+    "revision": "acc976b74aeeb85e4ce7",
+    "url": "/prototype/static/js/2.34666ca3.chunk.js"
+  },
+  {
+    "revision": "2abbcc0378228d296b9d",
+    "url": "/prototype/static/css/main.6f524674.chunk.css"
+  },
+  {
+    "revision": "acc976b74aeeb85e4ce7",
+    "url": "/prototype/static/css/2.dee41487.chunk.css"
+  },
+  {
+    "revision": "d1ab12b31410eed6fa91daa15a58e517",
+    "url": "/prototype/index.html"
+  }
+];
